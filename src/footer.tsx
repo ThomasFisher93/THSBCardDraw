64a1881e2e31db43274a0ced88b0b49ff80023df
ThomasFisher93 <tomfisher200693@gmail.com>
2020-12-27 16:24:54 +0700

    Add Music Pack Vol.3 songs

--- src/footer.tsx
+++ src/footer.tsx
@@ -9,7 +9,7 @@ import { TranslateContext } from "@denysvuika/preact-translate";
 import { About } from "./about";
 
 // note that month is zero-indexed for date constructor :)
-const lastUpdate = new Date(2020, 11, 17);
+const lastUpdate = new Date(2020, 11, 24);
 
 export function Footer() {
   const { t } = useContext(TranslateContext);
