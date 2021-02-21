72380d3f241f6a6d815bfca3b2443536ec11e012
ThomasFisher93 <tomfisher200693@gmail.com>
2021-02-21 18:14:56 +0700

    update th8 arrange songs & pcb arrange songs translation

--- src/footer.tsx
+++ src/footer.tsx
@@ -9,7 +9,7 @@ import { TranslateContext } from "@denysvuika/preact-translate";
 import { About } from "./about";
 
 // note that month is zero-indexed for date constructor :)
-const lastUpdate = new Date(2021, 1, 13);
+const lastUpdate = new Date(2021, 1, 18);
 
 export function Footer() {
   const { t } = useContext(TranslateContext);
