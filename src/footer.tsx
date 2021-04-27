feb01fde1f3001ed07fb3abaa6913ca12c9399c6
ThomasFisher93 <tomfisher200693@gmail.com>
2021-04-27 23:24:36 +0700

    add eurobeat pack songs

--- src/footer.tsx
+++ src/footer.tsx
@@ -9,7 +9,7 @@ import { TranslateContext } from "@denysvuika/preact-translate";
 import { About } from "./about";
 
 // note that month is zero-indexed for date constructor :)
-const lastUpdate = new Date(2021, 2, 18);
+const lastUpdate = new Date(2021, 3, 8);
 
 export function Footer() {
   const { t } = useContext(TranslateContext);
